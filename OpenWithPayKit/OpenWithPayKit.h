2931c12d00b46e3c5af2b6aa59af8e6cce229089
Creator William <creator_william@icloud.com>
2018-07-06 10:47:27 +0800

    Update

---
+++ OpenWithPayKit/OpenWithPayKit.h
@@ -0,0 +1,19 @@
+//
+//  OpenWithPayKit.h
+//  OpenWithPayKit
+//
+//  Created by William Lee on 2018/6/22.
+//  Copyright © 2018 William Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenWithPayKit.
+FOUNDATION_EXPORT double OpenWithPayKitVersionNumber;
+
+//! Project version string for OpenWithPayKit.
+FOUNDATION_EXPORT const unsigned char OpenWithPayKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWithPayKit/PublicHeader.h>
+
+
---
+++ OpenWithoutPayKit/OpenWithoutPayKit.h
@@ -0,0 +1,19 @@
+//
+//  OpenWithoutPayKit.h
+//  OpenWithoutPayKit
+//
+//  Created by William Lee on 2018/6/22.
+//  Copyright © 2018 William Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OpenWithoutPayKit.
+FOUNDATION_EXPORT double OpenWithoutPayKitVersionNumber;
+
+//! Project version string for OpenWithoutPayKit.
+FOUNDATION_EXPORT const unsigned char OpenWithoutPayKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OpenWithoutPayKit/PublicHeader.h>
+
+
